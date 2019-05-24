1bbfa987b6045f98d9cf59c023861e27da042868
Daniel Adiletta <dan.adiletta@gmail.com>
2019-05-24 11:13:55 -0400

    init

---
+++ ancestor.py
@@ -0,0 +1,27 @@
+import time
+import gopigo3
+from di_sensors.easy_distance_sensor import EasyDistanceSensor
+from di_sensors.inertial_measurement_unit import InertialMeasurementUnit
+
+class MrA(gopigo3.GoPiGo3):
+
+    def __init__(self):
+        super().__init__()
+        self.imu = InertialMeasurementUnit(bus = "GPG3_AD1") 
+        self.ds = EasyDistanceSensor()
+
+    def dist(self):
+        num = self.ds.read()
+        print("distance from object: {} mm".format(num))
+        return num
+
+    def angle(self):
+        num = self.imu.read_euler()[0]
+        print("currently facing angle: {} deg".format(num))
+        return num
+
+        
+
+
+
+    
---
+++ main.py
@@ -0,0 +1,6 @@
+from ancestor import MrA
+
+class Robot(MrA):
+
+    def __init__(self):
+        super().__init__()  
